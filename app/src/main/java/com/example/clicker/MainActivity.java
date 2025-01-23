e7139044cf0f2e691f7fe3ffaa59759bad50208b
unknown <operator@stacja42-02.szkola.zst-radom.edu.pl>
2025-01-23 09:18:03 +0100

    first commit

---
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.clicker;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;});
+
+            TextView Liczba = findViewById(R.id.Liczba);
+            Button button = findViewById(R.id.button);
+
+            button.setOnClickListener(view -> {
+                counter++;
+                Liczba.setText(String.valueOf(counter));
+
+    });
+}}
